0e85cf6796362ab128c199448693d5b943f86c20
Kelly Becker & Mark Lehman <student@epicodus.com>
2014-02-27 17:23:36 -0800

    adds functionality to display symbol in table when clicked, but isWin function not working

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,23 @@
 var Player = {
-  create: function(name) {
-    var player1 = Object.create(Player);
-    this.name = name;
-    return player1;
+  create: function() {
+    var newPlayer = Object.create(Player);
+    return newPlayer;
   },
 
-  // initialize: function(name) {
-  //   this.name = name;
-  // }
+  initialize: function(symbol) {
+    this.symbol = symbol;
+  }
 };
 
 var Space = {
-  create: function(xCoord, yCoord) {
+  create: function() {
     var newSpace = Object.create(Space);
-    this.xCoord = xCoord;
-    this.yCoord = yCoord;
     return newSpace;
   },
-  // initialize: function(xCoord, yCoord) {
-  //   this.xCoord = xCoord;
-  //   this.yCoord = yCoord;
-  // },
+  initialize: function(xCoord, yCoord) {
+    this.xCoord = xCoord;
+    this.yCoord = yCoord;
+  },
   markBy: function(XorO) {
     this.markedBy = XorO;
     return this.markedBy;
@@ -31,25 +28,61 @@ var Space = {
 var Board = {
   create: function() {
     var newBoard = Object.create(Board);
-    this.spaces = [];
+    //this.spaces = [];
     return newBoard;
   },
 
-  // initialize: function() {
-  //   this.spaces = [];
-  // }
+  initialize: function() {
+     this.spaces = [];
+  }
 };
 
 var Game = {
   create: function(turn) {
     var newGame = Object.create(Game);
-    this.turn = turn;
     return newGame;
   },
 
-  // initialize: function(turn) {
-  //   this.turn = turn;
-  // }
+  initialize: function(turn) {
+     this.turn = turn;
+     this.markedSpaces=[];
+  },
+
+  isWin: function(turn,markedSpaces) {
+    var wins = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+    if (turn % 2 === 0)
+      var symbolFinds = [];
+      var symbolFind = this.markedSpaces.indexOf("X");
+      console.log(symbolFinds);
+      while (symbolFind !== -1) {
+        symbolFinds.push(symbolFind);
+        symbolFind = symbolFinds.indexOf("X", symbolFind + 1);
+        
+      }
+      console.log(symbolFinds);
+//       for (var i = 0; i < 9; i++) {
+//         foundSymbol=symbolFinds.indexOf("X")
+//         symbolFinds.push(foundSymbol);
+//       }
+// console.log(symbolFinds);
+      // this.markedSpaces.forEach(function(space) {
+      //   if (space = "X") {
+      //     console.log(this.markedSpaces.indexOf(space));
+      //     // symbolFinds.push();
+      //   };
+      // });
+
+      
+      var winOrLose = wins.indexOf(symbolFinds);
+      //console.log(winOrLose);
+      if (winOrLose != -1) {
+        return true;
+      }
+
+    if (turn === 9) {
+      return false;  
+    }    
+  }
 };
 
 
@@ -62,10 +95,10 @@ $(document).ready(function() {
 
     var player1 = Player.create();
     player1.initialize("X");
-    
+
     var player2 = Player.create();
     player2.initialize("O");
-    
+  
     var newBoard = Board.create();
     newBoard.initialize([]);
 
@@ -74,31 +107,36 @@ $(document).ready(function() {
         var newSpace = Space.create();
         newSpace.initialize(r,c);
         newBoard.spaces.push(newSpace);
+        newGame.markedSpaces = newBoard.spaces;
       }
     }
-
-    $("#new-board").empty().append("<table class='table table-bordered'><tr><td id='space0'>" + newBoard.spaces[0] + 
-                    "</td><td id='space1'>" + newBoard.spaces[1] + 
-                    "</td><td id='space2'>" + newBoard.spaces[2] + 
-                    "</td></tr><tr><td id='space3'>" + newBoard.spaces[3] + 
-                    "</td><td id='space4'>" + newBoard.spaces[4] + 
-                    "</td><td id='space5'>" + newBoard.spaces[5] + 
-                    "</td></tr><tr><td id='space6'>" + newBoard.spaces[6] + 
-                    "</td><td id='space7'>" + newBoard.spaces[7] + 
-                    "</td><td id='space8'>" + newBoard.spaces[8] + 
-                    "</td></tr></table>"); 
-
-
-  });
-  if (newGame.turn % 2 === 0) {
-    //FOR LOOP??
+    $("#new-board").empty().append("<table class='table table-bordered'>" + 
+                    "<tr><td id='space0'><span></span></td>" + 
+                    "<td id='space1'><span></span></td>" + 
+                    "<td id='space2'><span></span></td></tr>" + 
+                    "<tr><td id='space3'><span></span></td>" + 
+                    "<td id='space4'><span></span></td>" + 
+                    "<td id='space5'><span></span></td></tr>" + 
+                    "<tr><td id='space6'><span></span></td>" + 
+                    "<td id='space7'><span></span></td>" + 
+                    "<td id='space8'><span></span></td></tr></table>"); 
+  
     $("#space0").click(function(event) {
-      if (newBoard.spaces[0] !== "X" || newBoard.spaces[0] !== "O") {
-        newBoard.spaces[0].markBy(player1);
-      } else {
-        alert("Ocupado. Choose again.")
-        return newBoard.spaces[0];
-      }
+      if (newGame.turn % 2 === 0) {
+        if ((newGame.markedSpaces[0] !== "X") && (newGame.markedSpaces[0] !== "O")) {
+          $("#space0").empty().append(player1.symbol);
+          newGame.markedSpaces[0]=player1.symbol;
+          newGame.turn++;
+          console.log(newGame.turn);
+          if (newGame.turn === 9) {
+            alert("Game Over");
+          }
+
+        } else {
+          alert("Ocupado. Choose again.")
+          //return newBoard.spaces[0];
+        };
+      };
     });
     $("#space1").click(function(event) {
     
@@ -124,6 +162,8 @@ $(document).ready(function() {
     $("#space8").click(function(event) {
     
     });
-  };
+  });
+
+  
 });
 
--- spec/specs.js
+++ spec/specs.js
@@ -1,50 +1,47 @@
 describe ("Player", function() {
   describe ("create", function() {
     it("creates a player", function() {
-      var testPlayer = Player.create("X");
+      var testPlayer = Player.create();
       Player.isPrototypeOf(testPlayer).should.equal(true);
 
-      testPlayer.name.should.equal("X");
     }); 
   });
-  // describe("initialize", function() {
-  //   it("initializes the Player object", function() {
-  //     var testPlayer = Object.create(Player);
-  //     testPlayer.initialize("X");
-  //     testPlayer.name.should.equal("X");
-  //   });
-  // });
+  describe("initialize", function() {
+    it("initializes the Player object", function() {
+      var testPlayer = Object.create(Player);
+      testPlayer.initialize("X");
+      testPlayer.symbol.should.equal("X");
+    });
+  });
 });
 
 describe("Space", function() {
   describe("create", function() {
     it("creates a Space object", function() {
-      var testSpace = Space.create(1,1);
+      var testSpace = Space.create();
       Space.isPrototypeOf(testSpace).should.equal(true);
 
-      testSpace.xCoord.should.equal(1);
-      testSpace.yCoord.should.equal(1);
     });
   });
 
-  // describe("initialize", function() {
-  //   it("initialize a new Space instance", function(){
-  //     var testSpace = Object.create(Space);
-  //     testSpace.initialize(1,1);
-  //     console.log(testSpace)
-  //     testSpace.xCoord.should.equal(1);
-  //     testSpace.yCoord.should.equal(1);
+  describe("initialize", function() {
+    it("initialize a new Space instance", function(){
+      var testSpace = Object.create(Space);
+      testSpace.initialize(1,1);
+      console.log(testSpace)
+      testSpace.xCoord.should.equal(1);
+      testSpace.yCoord.should.equal(1);
 
-  //   });
-  // });
+    });
+  });
 
   describe("markBy", function() {
     it("marks the players symbol in the space", function() {
       var testPlayer = Player.create("X");
       var testSpace = Space.create(2,0);
-      
+      console.log(testSpace)
       testSpace.markBy(testPlayer);
-
+      console.log(testSpace)
       testSpace.markedBy.should.equal(testPlayer);
     });
   });
@@ -53,36 +50,46 @@ describe("Space", function() {
 describe ("Board", function() {
   describe ("create", function() {
     it("creates a new Board", function() {
-      var testBoard = Board.create([]);
+      var testBoard = Board.create();
       Board.isPrototypeOf(testBoard).should.equal(true);
 
-      testBoard.spaces.should.eql([]);
     }); 
   });
 
-  // describe("initialize", function() {
-  //   it("initializes the Board object", function() {
-  //     var testBoard = Object.create(Board);
-  //     testBoard.initialize([]);
-  //     testBoard.spaces.should.eql([]);
-  //   });
-  // });
+  describe("initialize", function() {
+    it("initializes the Board object", function() {
+      var testBoard = Object.create(Board);
+      testBoard.initialize([]);
+      testBoard.spaces.should.eql([]);
+    });
+  });
 });
 
 describe("Game", function() {
   describe("create", function() {
     it("creates a new Game", function() {
-      var testGame = Game.create(0);
+      var testGame = Game.create();
       Game.isPrototypeOf(testGame).should.equal(true);
-      testGame.turn.should.equal(0);
     });
   });
 
-  // describe("initialize", function() {
-  //   it("initializes a new Game", function() {
-  //     var testGame = Object.create(Game);
-  //     testGame.initialize(0);
-  //     testGame.turn.should.equal(0);
-  //   });
-  // });
+  describe("initialize", function() {
+    it("initializes a new Game", function() {
+      var testGame = Object.create(Game);
+      testGame.initialize(0);
+      testGame.turn.should.equal(0);
+    });
+  });
+  describe("isWin", function() {
+    it("returns false after 9 turns", function() {
+      var testGame = Game.create();
+      testGame.initialize(9);
+      testGame.isWin(9).should.equal(false);
+    });
+    it("return true if win condition is met", function() {
+      var testGame = Game.create();
+      testGame.markedSpaces=["X","X","X",[1,0],"O",[1,2],"O",[2,1],[2,2]];
+      testGame.isWin(4,testGame.markedSpaces).should.equal(true);
+    });    
+  });
 });
